9499c0b1696188bf3cdd3840f26dce92a475c9db
Daria Puchalska <puchalska.daria@wp.pl>
2020-11-18 14:08:55 +0100

    Add evaluation and metrics visualization script

--- src/metrics.py
+++ src/metrics.py
@@ -8,6 +8,8 @@ def intersection_over_union(boxA, boxB) -> float:
     :param boxB: tuple in a form (upper_left_x, upper_left_y, width, height)
     :return: intersection over union
     """
+    if boxA[3] == 0 or boxB[3] == 0:
+        return None
     boxA_area = boxA[2] * boxA[3]
     boxB_area = boxB[2] * boxB[3]
     x_upper_left = max(boxA[0], boxB[0])
@@ -26,6 +28,8 @@ def area_ratio(bbox_true, bbox_pred):
     :param bbox_pred: tuple in a form (upper_left_x, upper_left_y, width, height)
     :return: area of bbox_pred / area of bbox_true
     """
+    if bbox_pred[3] == 0:
+        return None
     bbox_true_area = bbox_true[2] * bbox_true[3]
     bbox_pred_area = bbox_pred[2] * bbox_pred[3]
     return bbox_pred_area / bbox_true_area
