295ac4496b0ffc56c0ba43b410643484ad1bdeea
Daria Puchalska <puchalska.daria@wp.pl>
2020-12-01 13:01:47 +0100

    Add gitignore

--- test.py
+++ test.py
@@ -1,50 +1,14 @@
-import face_recognition
-import cv2
-import numpy as np
-from PIL import Image
+import pandas as pd
 
-LEFT_EYE_LOCATION = (0.35, 0.35)
-FACE_WIDTH = 252
-FACE_HEIGHT = 252
+data = pd.read_table('annotations/identity_CelebA.txt',
+                     delim_whitespace=True)
+print(data.identity.value_counts())
 
-# load an image as an arrary
-image = face_recognition.load_image_file("DSC_4121.jpeg")
-
-# detect faces from input image.
-locations = face_recognition.face_locations(image, model="hog")
-landmarks = face_recognition.face_landmarks(image)
-
-for (top, right, bottom, left), marks in zip(locations, landmarks):
-    image = cv2.rectangle(image, (left, bottom), (right, top), (255, 0, 0), 5)
-    left_eye = marks['left_eye']
-    right_eye = marks['right_eye']
-
-    left_eye_center = np.array(left_eye).mean(axis=0).astype("int")
-    right_eye_center = np.array(right_eye).mean(axis=0).astype("int")
-
-    left_eye_center = (left_eye_center[0], left_eye_center[1])
-    right_eye_center = (right_eye_center[0], right_eye_center[1])
-
-    dY = right_eye_center[1] - left_eye_center[1]
-    dX = right_eye_center[0] - left_eye_center[0]
-    angle = np.degrees(np.arctan2(dY, dX))
-    dist = np.sqrt((dX ** 2) + (dY ** 2))
-    new_right_eye_x = 1.0 - LEFT_EYE_LOCATION[0]
-    scale = (new_right_eye_x - LEFT_EYE_LOCATION[0]) * FACE_WIDTH / dist
-
-    center = ((left_eye_center[0] + right_eye_center[0]) // 2, (left_eye_center[1] + right_eye_center[1]) // 2)
-    rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
-
-    rotation_matrix[0, 2] += (FACE_WIDTH * 0.5 - center[0])
-    rotation_matrix[1, 2] += (FACE_HEIGHT * LEFT_EYE_LOCATION[1] - center[1])
-
-    # apply the affine transformation
-    output = cv2.warpAffine(image, rotation_matrix, (FACE_WIDTH, FACE_HEIGHT), flags=cv2.INTER_CUBIC)
-    encoding = face_recognition.face_encodings(output)[0]
-    print(encoding)
-
-image = cv2.resize(image, (860, 540))
-pil_image = Image.fromarray(image)
-pil_image.show()
-pil_image_face = Image.fromarray(output)
-pil_image_face.show()
+#image, locations, eyes = detect_faces(filepath)
+#top, right, bottom, left = locations[0]
+#image = cv2.rectangle(image, (left, bottom), (right, top), (255, 0, 0), 5)
+#matrix = get_rotation_matrix(eyes[0], LEFT_EYE_LOCATION, FACE_WIDTH, FACE_HEIGHT)
+#image_aligned = cv2.warpAffine(image, matrix, (FACE_WIDTH, FACE_HEIGHT), flags=cv2.INTER_CUBIC)
+#encoding = get_face_encoding(image_aligned)
+#pil_image = Image.fromarray(image)
+#pil_image.show()
